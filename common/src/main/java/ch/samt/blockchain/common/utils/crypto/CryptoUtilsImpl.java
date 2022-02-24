6d9a97a392a9ecf0b49975e2c5034bd0f5b8bc1d
Paolo Bettelini <59686810+paolobettelini@users.noreply.github.com>
2022-02-24 10:56:08 +0100

    key generator

--- common/src/main/java/ch/samt/blockchain/common/utils/crypto/CryptoUtilsImpl.java
+++ common/src/main/java/ch/samt/blockchain/common/utils/crypto/CryptoUtilsImpl.java
@@ -3,10 +3,15 @@ package ch.samt.blockchain.common.utils.crypto;
 public class CryptoUtils {
     
     private static SHA256Digest sha256Digest;
+    private static KeyPairGenerator ecKeyGen;
 
     static {
         try {
             sha256Digest = MessageDigest.getInstance("SHA-256");
+
+            KeyPairGenerator ecKeyGen = KeyPairGenerator.getInstance("EC");
+            ECGenParameterSpec ecSpec = new ECGenParameterSpec("secp256k1");
+            ecKeyGen.initialize(ecSpec);;
         } catch (NoSuchAlgorithmException | NoSuchPaddingException | NoSuchProviderException e) {
             e.printStackTrace();
         }
@@ -14,6 +19,12 @@ public class CryptoUtils {
     
     public static byte[] SHA256(byte[] digest) {
         return sha256Digest.digest(digest);
+    }
+
+    public static KeyPair generateKeyPair() {
+        KeyPair kp = ecKeyGen.generateKeyPair();
+        PublicKey pub = kp.getPublic();
+        PrivateKey pvt = kp.getPrivate()
     }
 
 }
