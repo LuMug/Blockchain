0d654ed206fafa3e3f1d26fcf8dd46fc1e6e3170
Paolo Bettelini <paolo.bettelini@samtrevano.ch>
2022-03-10 15:02:48 +0100

    web backend rest api

--- website-backend/src/main/java/ch/samt/blockchain/website_backend/Backend.java
+++ website-backend/src/main/java/ch/samt/blockchain/website_backend/Backend.java
@@ -7,7 +7,65 @@ public class Backend {
     public static void init(int port) {
         port(port);
         get("/get", (req, res) -> "Hello GET");
-        post("/post", (req, res) -> "Hello POST");
+        
+        // Get the latest 10 blocks
+        // /getLatestBlocks/0/10
+        post("/getLatestBlocks/:from/:to", (req, res) -> {
+        
+            int from = 0;
+            int to = 0;
+            
+            try {
+                from = Integer.parseInt(req.params(":from"));
+                to = Integer.parseInt(req.params(":to"));
+            } catch (NumberFormatException e) {
+                System.out.println("asdihu");
+                return "";
+            }
+
+            res.type("application/json");
+
+            /*res.body("""
+                {
+                    [
+                        { "id": 0, "hash": "aGFzaGRlbGJsb2Njb2hhc2hkZWxibG9jY28K", nTx: "59" },
+                        { "id": 1, "hash": "ZWFzdGVyZWdnZWFzdGVyZWdnZWFzdGVyQUFB", nTx: "32" },
+                        { "id": 2, "hash": "ZWRzdGVycmdnZWFmdGVyZWdzZWFzdmVyUkFB", nTx: "3" }
+                    ]
+                }
+            """
+            );*/
+            res.body("{\"CIAO\":1}");
+
+            return "CIAOOOO";
+
+        });
     }
-    
+
+    public static void stop() {
+        spark.Spark.stop();
+    }
+
+    /*
+    POST in JS
+
+    async function postData(url = '', data = {}) {
+        const response = await fetch(url, {
+        method: 'POST',
+        cache: 'no-cache',
+        headers: {
+            'Content-Type': 'application/json'
+        },2
+        referrerPolicy: 'no-referrer',
+            body: JSON.stringify(data)
+        });
+        return response;
+    }
+
+    postData('/getLatestBlocks/0/10', { })
+    .then(data => {
+        console.log(data);
+    });
+    */
+
 }
--- website-backend/src/main/java/ch/samt/blockchain/website_backend/Main.java
+++ website-backend/src/main/java/ch/samt/blockchain/website_backend/Main.java
@@ -1,9 +1,20 @@
 package ch.samt.blockchain.website_backend;
 
+import java.util.Scanner;
+
 public class Main {
     
     public static void main(String[] args) {
         Backend.init(7777);
+
+        // terminal
+        new Thread(() -> {
+            try (Scanner scanner = new Scanner(System.in)) {
+                switch (scanner.next()) {
+                    case "exit", "stop" -> Backend.stop();
+                }
+            }
+        }).start();
     }
 
 }
