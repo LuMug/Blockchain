1e948d16862e67b1104d94564dff08520c9e8f4f
giannigrasso <gianni.grasso@samtrevano.ch>
2022-04-14 10:58:53 +0200

    Website Update.

--- website-backend/src/main/java/ch/samt/blockchain/website_backend/Main.java
+++ website-backend/src/main/java/ch/samt/blockchain/website_backend/Main.java
@@ -8,13 +8,13 @@ public class Main {
 
     public static final int PORT = 80;
 
-    public static final String PATH = "/home/paolo/Scrivania/blockchain/website-frontend";
+    public static final String PATH = "C:\\Users\\giann\\OneDrive\\Documenti\\GitHub\\Blockchain\\website-frontend";
 
     public static void main(String[] args) {
         var http = Service
-            .ignite()
-            .port(PORT);
-        
+                .ignite()
+                .port(PORT);
+
         if (args.length != 2) {
             System.out.println("You can enable SSL/HTTP by");
             System.out.println("java -jar backend-jar <keystore.jks> <password>");
