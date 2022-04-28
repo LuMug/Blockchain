b8144873e7867de0baf1071b7d136a9f4df5fbf6
giannigrasso <gianni.grasso@samtrevano.ch>
2022-04-28 11:13:45 +0200

    Website functions

--- website-frontend/js/deploy.js
+++ website-frontend/js/deploy.js
@@ -1,5 +1,5 @@
 "use strict";
 
-        // https://www.smashingmagazine.com/2018/01/drag-drop-file-uploader-vanilla-js/
+// https://www.smashingmagazine.com/2018/01/drag-drop-file-uploader-vanilla-js/
 
 const dropzone = new Dropzone("#dropzone", { url: href + ":" + port + '/deploy' });
--- website-frontend/js/index.js
+++ website-frontend/js/index.js
@@ -1,8 +1,27 @@
 function searchWallet() {
     let temp = document.createElement('a');
     let input = document.getElementById("wallet").value;
-    alert(input);
     temp.href = "wallet.html?address=" + input;
+    temp.click();
+
+    update()
+    setInterval(update, 30000);
+}
+
+function searchTransaction() {
+    let temp = document.createElement('a');
+    let input = document.getElementById('transaction').value;
+    temp.href = "transaction.html?hash=" + input;
+    temp.click();
+
+    update()
+    setInterval(update, 30000);
+}
+
+function searchBlock() {
+    let temp = document.createElement('a');
+    let input = document.getElementById('block').value;
+    temp.href = "block.html?id=" + input;
     temp.click();
 
     update()
